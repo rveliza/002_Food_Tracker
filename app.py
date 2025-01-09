2b72525d986aa57ab809fc6da2884fe15a357de6
Reyner Veliz <reyner@reynerveliz.com>
2025-01-09 10:13:06 -0600

    Database helpers

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
+import sqlite3
 
 app = Flask(__name__)
+
+def connect_db():
+    # "Connect to the actual db file"
+    sql= sqlite3.connect('food_log.db')
+    sql.row_factory = sqlite3.Row # Results will be returned as dictionaries instead of tuples
+    return sql
+
+def get_db():
+    # "Check global object g if sqlite3_db exists"
+    if not hasattr(g, 'sqlite3_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
 
 @app.route('/')
 def index():
